68593baf9e2ae8775f4654ce0d4385fa5e8e9c67
DavidEnriqueNieves <davidenriquenieves@gmail.com>
2023-05-01 16:25:51 -0400

    was using ln instead of log10

--- equal_coeff_fmin_prototype.m
+++ equal_coeff_fmin_prototype.m
@@ -34,117 +34,117 @@ options = optimoptions('fmincon', 'MaxIterations', 5000, 'Algorithm' , 'active-s
 
 % Display results
 fprintf('Optimal Values:\n');
-fprintf('P2G: %.2f\n', x(1));
-fprintf('P5G: %.2f\n', x(2));
-fprintf('D2G: %.2f\n', x(3));
-fprintf('D5G: %.2f\n', x(4));
-fprintf('I2G: %.2f\n', x(5));
-fprintf('I5G: %.2f\n', x(6));
+fprintf('D2G: %.2f\n', x(1));
+fprintf('D5G: %.2f\n', x(2));
+fprintf('I2G: %.2f\n', x(3));
+fprintf('I5G: %.2f\n', x(4));
+fprintf('P2G: %.2f\n', x(5));
+fprintf('P5G: %.2f\n', x(6));
 
-file_name = strcat("~/Documents/Semester10/MTH5335/Project/results(algorithm="+ options.Algorithm+ "+ iterations="+ options.MaxIterations+  "f=" + fval+ ".csv" );
-
-
-results = write_results_to_csv(file_name, x, fval, x0, ub, lb, exitflag, output, lambda, grad, hessian, C2G, C5G, f_coeffs);
-
-% Define function to write results to a CSV file
-function results = write_results_to_csv(file_name, x_opt, fval, x0, ub, lb, exitflag, output, lambda, grad, hessian, C2G, C5G, f_coeffs)
-
-% Create cell array to store results
-results = cell(21, 8);
-
-results{1, 1} = "TABLE";
-% Fill in results cell array
-results{2, 1} = "D2G";
-results{3, 1} = "D5G";
-results{4, 1} = "I2G";
-results{5, 1} = "I5G";
-results{6, 1} = "P2G";
-results{7, 1} = "P5G";
-
-results{1, 2} = 'x'
-results{2, 2} = x_opt(1);
-results{3, 2} = x_opt(2);
-results{4, 2} = x_opt(3);
-results{5, 2} = x_opt(4);
-results{6, 2} = x_opt(5);
-results{7, 2} = x_opt(6);
-
-results{1, 3} = 'x_0'
-results{2, 3} = x0(1);
-results{3, 3} = x0(2);
-results{4, 3} = x0(3);
-results{5, 3} = x0(4);
-results{6, 3} = x0(5);
-results{7, 3} = x0(6);
-
-results{1, 4} = 'UB'
-results{2, 4} = ub(1);
-results{3, 4} = ub(2);
-results{4, 4} = ub(3);
-results{5, 4} = ub(4);
-results{6, 4} = ub(5);
-results{7, 4} = ub(6);
-
-results{1, 5} = 'LB'
-results{2, 5} = lb(1);
-results{3, 5} = lb(2);
-results{4, 5} = lb(3);
-results{5, 5} = lb(4);
-results{6, 5} = lb(5);
-results{7, 5} = lb(6);
+suffix = "";
+file_name = strcat("~/Documents/Semester10/MTH5335/Project/results(algorithm="+ options.Algorithm+ "+iterations="+ options.MaxIterations+  "f=" + fval+ ")" + suffix + ".csv" );
 
 
-results{9, 1} = 'F-value';
-results{10, 1} = fval;
-
-results{9, 2} = 'Iterations';
-results{10, 2} = output.iterations;
-
-results{9, 3} = 'Algorithm';
-results{10, 3} = output.algorithm;
-
-results{9, 4} = 'FuncCount';
-results{10, 4} = output.funcCount;
-
-results{9, 5} = 'StepSize';
-results{10, 5} = output.stepsize;
-
-
-% if eigenvalues of hessian are greater than or equal to 0
-
-results{9, 6} = "H >= 0";
-results{10, 6} = string(all(eig(hessian) >= -0.001));
-results{9, 7} = "H > 0 ";
-results{10, 7} = string(all(eig(hessian) >= 0));
-
-results{12, 2} = "Chunk Size 2G";
-results{13, 2} = C2G;
-
-results{12, 3} = "Chunk Size 5G";
-results{13, 3} = C5G;
-
-results{14, 1} = "Grad";
-results{15, 1} = grad(1);
-results{16, 1} = grad(2);
-results{17, 1} = grad(3);
-results{18, 1} = grad(4);
-results{19, 1} = grad(5);
-results{20, 1} = grad(6);
-
-results{1, 5} = "$f(x)$ Coeffs";
-results{2, 5} = f_coeffs(1);
-results{3, 5} = f_coeffs(2);
-results{4, 5} = f_coeffs(3);
-results{5, 5} = f_coeffs(4);
-results{6, 5} = f_coeffs(5);
-results{7, 5} = f_coeffs(6);
-% Write results to CSV file
-% writematrix( results,file_name);
-cell2csv(file_name,results)
-% csvwrite(file_name, results);
-
+write_results_to_csv(file_name, x, fval, x0, ub, lb, exitflag, output, lambda, grad, hessian, C2G, C5G, f_coeffs);
+	
+	% Define function to write results to a CSV file
+	function write_results_to_csv(file_name, x_opt, fval, x0, ub, lb, exitflag, output, lambda, grad, hessian, C2G, C5G, f_coeffs)
+	
+	% Create cell array to store results
+	results = cell(21, 8);
+	
+	results{1, 1} = "TABLE";
+	% Fill in results cell array
+	results{2, 1} = "D2G";
+	results{3, 1} = "D5G";
+	results{4, 1} = "I2G";
+	results{5, 1} = "I5G";
+	results{6, 1} = "P2G";
+	results{7, 1} = "P5G";
+	
+	results{1, 2} = 'x'
+	results{2, 2} = x_opt(1);
+	results{3, 2} = x_opt(2);
+	results{4, 2} = x_opt(3);
+	results{5, 2} = x_opt(4);
+	results{6, 2} = x_opt(5);
+	results{7, 2} = x_opt(6);
+	
+	results{1, 3} = 'x_0'
+	results{2, 3} = x0(1);
+	results{3, 3} = x0(2);
+	results{4, 3} = x0(3);
+	results{5, 3} = x0(4);
+	results{6, 3} = x0(5);
+	results{7, 3} = x0(6);
+	
+	results{1, 4} = 'UB'
+	results{2, 4} = ub(1);
+	results{3, 4} = ub(2);
+	results{4, 4} = ub(3);
+	results{5, 4} = ub(4);
+	results{6, 4} = ub(5);
+	results{7, 4} = ub(6);
+	
+	results{1, 5} = 'LB'
+	results{2, 5} = lb(1);
+	results{3, 5} = lb(2);
+	results{4, 5} = lb(3);
+	results{5, 5} = lb(4);
+	results{6, 5} = lb(5);
+	results{7, 5} = lb(6);
+	
+	
+	results{9, 1} = 'F-value';
+	results{10, 1} = fval;
+	
+	results{9, 2} = 'Iterations';
+	results{10, 2} = output.iterations;
+	
+	results{9, 3} = 'Algorithm';
+	results{10, 3} = output.algorithm;
+	
+	results{9, 4} = 'FuncCount';
+	results{10, 4} = output.funcCount;
+	
+	results{9, 5} = 'StepSize';
+	results{10, 5} = output.stepsize;
+	
+	
+	% if eigenvalues of hessian are greater than or equal to 0
+	
+	results{9, 6} = "H >= 0";
+	results{10, 6} = string(all(eig(hessian) >= -0.001));
+	results{9, 7} = "H > 0 ";
+	results{10, 7} = string(all(eig(hessian) >= 0));
+	
+	results{12, 2} = "Chunk Size 2G";
+	results{13, 2} = C2G;
+	
+	results{12, 3} = "Chunk Size 5G";
+	results{13, 3} = C5G;
+	
+	results{14, 1} = "Grad";
+	results{15, 1} = grad(1);
+	results{16, 1} = grad(2);
+	results{17, 1} = grad(3);
+	results{18, 1} = grad(4);
+	results{19, 1} = grad(5);
+	results{20, 1} = grad(6);
+	
+	results{1, 5} = "$f(x)$ Coeffs";
+	results{2, 5} = f_coeffs(1);
+	results{3, 5} = f_coeffs(2);
+	results{4, 5} = f_coeffs(3);
+	results{5, 5} = f_coeffs(4);
+	results{6, 5} = f_coeffs(5);
+	results{7, 5} = f_coeffs(6);
+	% Write results to CSV file
+	% writematrix( results,file_name);
+	cell2csv(file_name,results)
+	% csvwrite(file_name, results);
+	
 end
-
 
 function cell2csv(filename,cellArray,delimiter)
 % Writes cell array content into a *.csv file.
@@ -187,19 +187,14 @@ fclose(datei);
 
 end
 
-
-
 function [c,ceq] = nothing(x)
     c(1) = 0;
     c(2) = 0;
     ceq = [];
 end
-
-
 function id = identity(x)
         id = x;
 end
-
 
 % Define packet_loss function
 function pl = packet_loss(distance)
@@ -240,7 +235,8 @@ function s = average_speed(c, i,distance, freq)
 end
 
 % Define objective function
-function y = f(P2G, P5G, D2G, D5G, I2G, I5G, f_coeffs)
+% function y = f(P2G, P5G, D2G, D5G, I2G, I5G, f_coeffs)
+function y = f( D2G, D5G, I2G, I5G,P2G, P5G, f_coeffs)
     max_power = 8;
     min_2G_path_loss = 3.60435984234;
     min_5G_path_loss = 3.6220886093;
@@ -252,8 +248,27 @@ function y = f(P2G, P5G, D2G, D5G, I2G, I5G, f_coeffs)
     C5G = 2048;
     total_distance = D2G*P2G / (200 * 8) + D5G*P5G / (60 * 8);
     number_of_phones = P2G*P5G / (8 * 8);
-    free_space_path_loss =  (  log(4*pi*2.4*G_over_c*D2G) - min_2G_path_loss ) / ( max_2G_path_loss - min_2G_path_loss )  
-    		         +  (  log(4*pi*5*G_over_c*D5G) - min_5G_path_loss ) / ( max_5G_path_loss - min_5G_path_loss );
+    
+    disp("D2G");
+    disp(D2G);
+    disp("D5G");
+    disp(D5G);
+
+    disp(" max_5G_path_loss - min_5G_path_loss ");
+    disp(max_5G_path_loss - min_5G_path_loss);
+
+    disp(" max_2G_path_loss - min_2G_path_loss ");
+    disp(max_2G_path_loss - min_2G_path_loss);
+
+    disp(" log(4*pi*2.4*G_over_c*D2G) - min_2G_path_loss ");
+    disp(log(4*pi*2.4*G_over_c*D2G) - min_2G_path_loss);
+
+    disp(" log(4*pi*5*G_over_c*D5G) - min_5G_path_loss ");
+    disp(log(4*pi*5*G_over_c*D5G) - min_5G_path_loss);
+
+    free_space_path_loss =  (  log10(4*pi*2.4*G_over_c*D2G) - min_2G_path_loss ) ./ ( max_2G_path_loss - min_2G_path_loss )  
+    		         +  (  log10(4*pi*5*G_over_c*D5G) - min_5G_path_loss ) ./ ( max_5G_path_loss - min_5G_path_loss );
+
 
     % bytes_lost = C2G * packet_loss(D2G) + C5G * packet_loss(D5G);
     bytes_lost = 0
@@ -267,7 +282,19 @@ function y = f(P2G, P5G, D2G, D5G, I2G, I5G, f_coeffs)
 
     transfer_speed = ( average_speed(C2G,I2G,D2G, 2.4) - min_2G_speed ) / (max_2G_speed - min_2G_speed) + ( average_speed(C5G,I5G,D5G,  5) - min_5G_speed ) / (max_5G_speed - min_5G_speed);
 
-    power_savings = I2G*P2G / max_power + I5G*P5G / max_power;
+    
+    disp("I2G");
+    disp(I2G);
+    disp("I5G");
+    disp(I5G);
+    disp("P2G");
+    disp(P2G);
+    disp("P5G");
+    disp(P5G);
+    disp("max_power");
+    disp(max_power);
+
+    power_savings = ( (I2G*P2G) / max_power ) + ( (I5G*P5G) / max_power );
 
     power_savings_coeff = f_coeffs(1);
     speed_coeff = f_coeffs(2);
@@ -275,6 +302,19 @@ function y = f(P2G, P5G, D2G, D5G, I2G, I5G, f_coeffs)
     bytes_lost_coeff = f_coeffs(4);
     number_of_phones_coeff = f_coeffs(5);
     total_distance_coeff = f_coeffs(6);
+
+    disp("total_distance")
+    disp(total_distance)
+    disp("number_of_phones")
+    disp(number_of_phones)
+    disp("free_space_path_loss")
+    disp(free_space_path_loss)
+    disp("bytes_lost")
+    disp(bytes_lost)
+    disp("transfer_speed")
+    disp(transfer_speed)
+    disp("power_savings")
+    disp(power_savings)
 y = power_savings_coeff * power_savings + ...
   speed_coeff * transfer_speed + ...
   path_loss_coeff * free_space_path_loss + ...
@@ -282,16 +322,4 @@ y = power_savings_coeff * power_savings + ...
   number_of_phones_coeff * number_of_phones + ...
   total_distance_coeff * total_distance;
 end
-
-% Define clip function
-function x = clip(x)
-    x(1) = min(max(x(1), 0), 8);
-    x(2) = min(max(x(2), 0), 8);
-    x(3) = min(max(x(3), 40), 200);
-    x(4) = min(max(x(4), 20), 60);
-    x(5) = min(max(x(5), 0), 1);
-    x(6) = min(max(x(6), 0), 1);
-    x = x;
-end
-
 
